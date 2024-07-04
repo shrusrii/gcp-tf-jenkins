fee332c07215b20d967d824d4fc95cc77337a2c0
Shruthinb <36254551+Shruthinb@users.noreply.github.com>
2024-07-04 22:27:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,10 @@ terraform {
   required_version = "~> 1.2"
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version  "1.2"
-    }
-
-    google-beta = {
-      source = "hashicorp/google-beta"
-      version ">~4.40.0"
+      version = "~> 4.0.0"
     }
   }
 }
-
 
 
 resource "google-storage-bucket" "my-bucket"{

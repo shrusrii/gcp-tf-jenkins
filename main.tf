e920ff8e8377b7482a5603e68f7be2ecea9a85f4
Shruthinb <36254551+Shruthinb@users.noreply.github.com>
2024-07-04 22:33:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_version = "~> 1.2"
-  required_providers {
-    google = {
-      version = "~> 4.0.0"
-    }
-  }
-}
-
 resource "google-storage-bucket" "my-bucket"{
 name = "githubdemo-bucket"
 project = "myfirstproject"

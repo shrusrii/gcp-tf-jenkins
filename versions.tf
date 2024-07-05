aaf4cb002ec9e3320f87df2752474931b0e430e6
Shruthinb <36254551+Shruthinb@users.noreply.github.com>
2024-07-05 16:12:40 +0530

    Delete versions.tf

--- versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.85.0"  # Adjust the version constraint as needed
-    }
-  }
-}

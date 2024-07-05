1a51643e74b15461ed04bd9f793956e87af50758
Shruthinb <36254551+Shruthinb@users.noreply.github.com>
2024-07-05 16:06:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project     = "bionic-trilogy-428106-h5"
-}
-
-
 resource "google_storage_bucket" "my-bucket"{
 name = "sr-githubdemo-bucket"
 project = "bionic-trilogy-428106-h5"

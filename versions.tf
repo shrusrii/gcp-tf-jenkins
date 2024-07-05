81f8d7d1ff3a169f5988e79a63eaa260c42f4968
Shruthinb <36254551+Shruthinb@users.noreply.github.com>
2024-07-05 15:39:36 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "~> 4.0"  # Adjust the version constraint as needed
+      version = "4.85.0"  # Adjust the version constraint as needed
     }
   }
 }

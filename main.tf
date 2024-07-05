e56ec0e7393806af908ab1a92aef9ef73783f1fc
Shruthinb <36254551+Shruthinb@users.noreply.github.com>
2024-07-05 16:14:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my-bucket"{
-name = "sr-githubdemo-bucket"
+name = "srw-githubdemo-bucket"
 project = "bionic-trilogy-428106-h5"
 location = "US"
 force_destroy = true

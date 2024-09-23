cb76427ce8d9d4ed0c8f0626d26196142339d6e1
Shruthinb <36254551+Shruthinb@users.noreply.github.com>
2024-09-23 16:02:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket"{
 name = "srw-githubdemo-bucket"
 project = "bionic-trilogy-428106-h5"
-location = "US"
+location = "UK"
 force_destroy = true
 public_access_prevention = "enforced"
 }
